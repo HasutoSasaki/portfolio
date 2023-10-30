818487d9f374968061326fe935c11516346a14da
hasuto <96404939+hasuto@users.noreply.github.com>
2023-10-31 07:50:19 +0900

    ヘッダーの通知マークとプロフィールアイコンを無くしました

--- plugins/apollo.js
+++ plugins/apollo.js
@@ -1,5 +1,4 @@
 export default defineNuxtPlugin((nuxtApp) => {
-    console.log(process.env.GITHUB_TOKEN)
     const { githubToken } = useRuntimeConfig();
     nuxtApp.hook('apollo:auth', ({ client, token }) => {
         token.value = githubToken;
